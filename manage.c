e215db1324509d110c4f279f8a56c7c57a7bddd2
chawj1234 <84870285+chawj1234@users.noreply.github.com>
2022-05-06 12:33:58 +0900

    Update manage.c

--- manage.c
+++ manage.c
@@ -10,7 +10,8 @@ int selectMenu(){
 	printf("3. 수정\n");
 	printf("4. 삭제\n");
 	printf("5. 저장\n");
-	printf("6. 이름검색\n");
+	printf("6. 배송 유무 검색\n");
+	printf("7. 가격 정렬(오름차순) 검색\n");
 	printf("0. 종료\n\n");
 	printf("=> 원하는 메뉴는? ");
 	scanf("%d", &menu);
