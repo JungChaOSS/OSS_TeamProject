7909dbedbb3fb2f73068ec6ff004e4ce766ad632
chawj1234 <84870285+chawj1234@users.noreply.github.com>
2022-05-06 12:31:01 +0900

    Create manage.c

---
+++ manage.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "manager.h"
+
+int selectMenu(){
+	int menu;
+	printf("\n**********\n");
+	printf("1. 조회\n");
+	printf("2. 추가\n");
+	printf("3. 수정\n");
+	printf("4. 삭제\n");
+	printf("5. 저장\n");
+	printf("6. 이름검색\n");
+	printf("0. 종료\n\n");
+	printf("=> 원하는 메뉴는? ");
+	scanf("%d", &menu);
+    getchar();
+	return menu;
+}
